518313f85a0888e1ea810e028ab79605c894f3c8
unknown <you@example.com>
2022-01-19 15:53:32 +0600

    private routing using v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
 import Posts from './Components/Posts';
 import Nav from './Components/Nav';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SinglePost from './Components/SinglePost';
+import Login from './Components/Login';
+import PrivateRoute from './Components/PrivateRoute';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Nav></Nav>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/post" element={<Posts />}></Route>
+          <Route path="/post" element={<PrivateRoute><Posts /></PrivateRoute>}></Route>
+          <Route path="/login" element={<Login />} />
           <Route path="/post/:id" element={<SinglePost></SinglePost>}></Route>
         </Routes>
       </BrowserRouter>
